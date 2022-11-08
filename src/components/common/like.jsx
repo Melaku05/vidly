ede8e3482823437f71b58ffd9ce92ad742c02a2b
melaku05 <melakushiferaw05@gmail.com>
2022-11-08 11:33:06 +0300

    like event handler first check

---
+++ src/components/common/like.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+// Input: liked: boolean
+// Output: onClick
+//when the user clicks on the heart icon, the liked property of the movie object is toggled
+// whenever we click on the heart icon, the onClick event handler is called
+
+class Like extends Component {
+    state = {
+
+    };
+    render() {
+        let classes = "fa fa-heart";
+        if (!this.props.liked) classes += "-o";
+        return (
+        <i
+            onClick={this.props.onClick}
+            style={{ cursor: "pointer" }}
+            className={classes}
+            aria-hidden="true"
+        />
+        );
+    }
+}
+    
+export default Like;
